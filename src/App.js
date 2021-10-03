6c6c2cc618e4a4c5a2224784e8b0fcc3889b21c2
Yessine <benhlima.yessine@gmail.com>
2021-10-03 14:54:22 +0100

    state

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    Person : {
+      fullName:"Yessine Ben Hlima",
+      bio:"30 years",
+      imgSrc:"./profilePhoto.jpg",
+      profession:"Engineer"},
+    show: false,    
+  };
+
+  handleClick=()=>{
+    this.setState({...this.state,
+      show: !this.state.show}
+      )
+    };
+
+      
+      
+  render(){
+        return (
+          <div className="App">
+            
+            <button onClick={this.handleClick} >{this.state.show?('Hidden'):('Show')}</button>
+            { this.state.show ? (
+              <div className="card">
+                <img src={this.state.Person.imgSrc} width="120px" height="120px" alt=""/>
+                <div className="person">My name is {this.state.Person.fullName}</div>
+                <div className="person">I`m {this.state.Person.bio} old</div>
+                <div className="person">I`m an {this.state.Person.profession}</div>                
+              </div>                           
+              ):(<h2>Click to show the profile!!</h2>) 
+            }  
+                          
+          </div>
+    );
+  }
+  
+}
+
+export default App;
