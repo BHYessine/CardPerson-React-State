df8cce8fbd54147470482ce2f2b3947536cd2c75
BHYessine <87872940+BHYessine@users.noreply.github.com>
2021-10-05 14:20:23 +0100

    Create Time.js

---
+++ src/Time.js
@@ -0,0 +1,23 @@
+import React from 'react';
+class Time extends React.Component {
+  state = {
+    digit: 0
+  };
+componentDidMount = () => {
+  this.myTimer = setInterval(() => {
+    this.setState((prevState) => ({
+      digit: prevState.digit + 1
+    }));
+ }, 1000);
+ };
+ 
+   componentWillUnmount = () => {
+     clearInterval(this.myTimer);
+   }
+   render(){
+    return (
+      <div> Counter : {this.state.digit} </div> 
+    );}
+  }
+
+   export default Time;
